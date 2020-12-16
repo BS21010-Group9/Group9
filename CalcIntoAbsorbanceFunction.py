5bc31afe2eae0edde2a1e611f2f88d19c1f26291
AlanaJohnson352 <75366787+AlanaJohnson352@users.noreply.github.com>
2020-12-16 11:46:04 +0000

    Added loop for list of values calculation.

--- CalcIntoAbsorbanceFunction.py
+++ CalcIntoAbsorbanceFunction.py
@@ -14,13 +14,25 @@ Created on Wed Dec  9 11:17:42 2020
 import math
 
 
+def intoabs(trans):    ##MAIN FUNCTION - calculation for Absorbance
+    absorbance_calc = 2 - (math.log(trans, 10))
+    conval.append(absorbance_calc)   ##adds values to new list
+    return(absorbance_calc)   
+
+def intotrans(absr):    ##MAIN FUNCTION - calc for Transmittance
+    transmittance_calc = (10**-absr)*100
+    conval.append(transmittance_calc)    #adds values to new list
+    return(transmittance_calc)
+
 
 values = []    #list of values to be converted
+
+conval = []    #list of values after conversion
 
 #maxlim, minlim and intervals needed. minlim = 0. maxlim = (length*interv)-interv!
 
 valuecount = int(input("How many values do you have?"))    #This is used to calculate the range of time, assuming the first reading was completed at 0 mins.
-
+#maybe make this float?
 interv = int(input("How much time did you leave between each reading? (please enter in minutes):"))
 
 maxlim = ((valuecount*interv)-interv)    #getting 0-highest time stamp
@@ -33,48 +45,38 @@ while tora not in ("t", "a"):
         for i in range(0,valuecount):    #addition of 0 allows range to be of the right length. (usually excludes end number)
             absr = float(input("Please enter each value in order, starting from the first."))    #user inputs values they want to convert.
             values.append(absr)    #Absorbance readings are added to the list values[]
-        
+        for i in values:    #for loop applies function to all values in list.
+            intoabs(i)    #applies the function to convert Transmittance to Absorbance.
+                            
     elif tora == "a":
         for i in range(0,valuecount):    #addition of 0 allows range to be of the right length. (usually excludes end number)
             trans = float(input("Please enter each value in order, starting from the first."))
             values.append(trans)    #Transmittance readings added to list
+        for i in values:    #for loop applies function to all values in list.
+            intotrans(i)    #applies function
    
     else:
-        print("Please respond with either 't' or 'a'.")    #avoids error, ensures user provides appropriate input.
+        print("Please respond with either 't' or 'a'.")    #avoids error, ensures user provides appropriate input.   #avoids error, ensures user provides appropriate input.
 #these two chunks of code need linked together,
 #ive added ala's transmittance & absorbance calculations
 #into the same program.
 #need a for loop & counter in this calculation area to go through the whole list 
 #Alicja's code- Calculations
 
-transmittance = float(input("Please type in  your transmittance value here:"))
-
-#comment code
-def intoabs(tran = transmittance):
-    absorbance_calc = 2 - (math.log(tran, 10))
-    return(absorbance_calc)
-
-<<<<<<< HEAD
-#calculation for transmittance
-def intotrans(absr):
-    transmittance_calc = (10**-absr)*100
-    return(transmittance_calc)
 
 #choosing which function to run based on intended calculation
-if tora == "t":
-    print(intoabs(absr))
-    
-else:
-    print(intotrans(absr))
+print(conval)
 
 
 ###
+r = int()
+v = int()
 #Judith's code
 def PrintAllValues ():
     output = 'You have introduced {} values.'.format (valuecount)
     output_2 =' You left {} minutes between each reading.'.format (interv)
     output_3 ='These are your time stamps with each converted value:'
-    for r, v in zip(range(0,maxlim+interv,interv), values):#maxlim+interv, as range() excludes the end number provided, but we still want to include max.
+    for r, v in zip(range(0,maxlim+interv,interv), conval):#maxlim+interv, as range() excludes the end number provided, but we still want to include max.
         output_4 = (r, "mins", v)
         print (r, "mins:", v)
     return (output, output_2, output_3, output_4)
@@ -88,6 +90,7 @@ def SaveResults ():
     f.write (str(PrintAllValues ()))
     f.close()
     
+    #save function
 ask_user = str(input('Would you like to save the results?'))
 yes = 'yes'
 if ask_user == yes:
@@ -95,19 +98,20 @@ if ask_user == yes:
 else:
     print ('Okay. Goodbye!')
     
-=======
-absorbance = float(input("Please type in your absorbance value here:"))
->>>>>>> refs/remotes/origin/main
+    
+#=======
+#absorbance = float(input("Please type in your absorbance value here:"))
+#>>>>>>> refs/remotes/origin/main
 
-def intotrans(absr = absorbance):
-    transmittence_calc = (10**-absr)*100
-    return(transmittence_calc)
+#def intotrans(absr = absorbance):
+#    transmittence_calc = (10**-absr)*100
+#    return(transmittence_calc)
 
-print(absorbance_calc, transmittence_calc)
+#print(absorbance_calc, transmittence_calc)
 
-<<<<<<< HEAD
-  return (output, output_2, output_3, output_4)
+#<<<<<<< HEAD
+#  return (output, output_2, output_3, output_4)
 
 
-=======
->>>>>>> refs/remotes/origin/main
+#=======
+#>>>>>>> refs/remotes/origin/main
