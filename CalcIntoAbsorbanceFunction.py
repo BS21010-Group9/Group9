d34bc47765a220c4e1e675a8e6c2079b34f12552
Marianne Khalil <75686975+marik0201@users.noreply.github.com>
2020-12-18 18:44:00 +0000

    Update CalcIntoAbsorbanceFunction.py

--- CalcIntoAbsorbanceFunction.py
+++ CalcIntoAbsorbanceFunction.py
@@ -24,7 +24,7 @@ def intotrans(absr):    ##MAIN FUNCTION - calc for Transmittance
     conval.append(transmittance_calc)    #adds values to new list
     return(transmittance_calc)
 
-
+ ##PRIMARY FUNCTIONS
 values = []    #list of values to be converted
 
 conval = []    #list of values after conversion
@@ -33,39 +33,33 @@ conval = []    #list of values after conversion
 
 valuecount = int(input("How many values do you have?"))    #This is used to calculate the range of time, assuming the first reading was completed at 0 mins.
 #maybe make this float?
-interv = int(input("How much time did you leave between each reading? (please enter in minutes):"))
+interv = int(input("How much time did you leave between each reading? (please enter in minutes):"))    #used to generate time stamp sequence
 
 maxlim = ((valuecount*interv)-interv)    #getting 0-highest time stamp
 
-tora = ""    #setting the variable for the while loop that asks the user which (T or A) they would like to calculate.
+tora = ""    #setting the variable for the input function that asks the user which (T or A) they would like to calculate.
 
-while tora not in ("t", "a"):
+while tora not in ("t", "a"):    #while loop ensures program continues running if appropriate input is not provided.
     tora = input("Would you like to calculate Transmittance or Absorbance? Please enter t/a:") 
-    if tora == "t":
+    if tora == "t":    #if user chooses to calculate Transmittance
         for i in range(0,valuecount):    #addition of 0 allows range to be of the right length. (usually excludes end number)
-            absr = float(input("Please enter each value in order, starting from the first."))    #user inputs values they want to convert.
+            absr = float(input("Please enter each Absorbance value in order, starting from the first."))    #user inputs values they want to convert.
             values.append(absr)    #Absorbance readings are added to the list values[]
         for i in values:    #for loop applies function to all values in list.
-            intoabs(i)    #applies the function to convert Transmittance to Absorbance.
+            intoabs(i)    #MAIN FUNCTION applied to convert Transmittance to Absorbance.
                             
-    elif tora == "a":
+    elif tora == "a":    #if user chooses to calculate Absorbance
         for i in range(0,valuecount):    #addition of 0 allows range to be of the right length. (usually excludes end number)
-            trans = float(input("Please enter each value in order, starting from the first."))
+            trans = float(input("Please enter each Transmittance value in order, starting from the first."))
             values.append(trans)    #Transmittance readings added to list
         for i in values:    #for loop applies function to all values in list.
-            intotrans(i)    #applies function
+            intotrans(i)    #MAIN FUNCTION applied function
    
     else:
-        print("Please respond with either 't' or 'a'.")    #avoids error, ensures user provides appropriate input.   #avoids error, ensures user provides appropriate input.
-#these two chunks of code need linked together,
-#ive added ala's transmittance & absorbance calculations
-#into the same program.
-#need a for loop & counter in this calculation area to go through the whole list 
-#Alicja's code- Calculations
+        print("Please respond with either 't' or 'a'.")    #avoids error, ensures user provides appropriate input.
 
 
 #choosing which function to run based on intended calculation
-print(conval)
 
 
 ###
@@ -98,7 +92,7 @@ def SaveResults ():
     f.close()
     
     #save function
-ask_user = str(input('Would you like to save the results?'))
+ask_user = str(input('Would you like to save the results? Please enter yes/no'))
 yes = 'yes'
 if ask_user == yes:
     SaveResults()
